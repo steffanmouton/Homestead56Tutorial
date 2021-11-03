29f56bb0e73924966870a0b1659af0ae63c351d8
Steffan Mouton <smouton8993@gmail.com>
2021-11-03 16:54:34 -0500

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+    <h1>
+        Welcome!!
+    </h1>
+    <p>
+        <a href="/contact">Contact</a> us to learn more.
+    </p>
+</body>
+
+</html>
