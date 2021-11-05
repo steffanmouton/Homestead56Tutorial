fa43771e5808e69d858f60c35ed34cdb466dcecc
Steffan Mouton <smouton8993@gmail.com>
2021-11-05 17:36:32 -0500

    Added Create route

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@
 Route::get('/', 'PagesController@home');
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
+
 Route::get('/projects', 'ProjectsController@index');
+Route::get('/projects/create', 'ProjectsController@create');
